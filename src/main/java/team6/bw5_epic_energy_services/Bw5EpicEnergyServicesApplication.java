0f7b7e91b451501de2df7728470d460d3287440e
IC Tech <148203290+ivancroce@users.noreply.github.com>
2025-08-04 11:01:55 +0100

    Initial commit

---
+++ src/main/java/team6/bw5_epic_energy_services/Bw5EpicEnergyServicesApplication.java
@@ -0,0 +1,13 @@
+package team6.bw5_epic_energy_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bw5EpicEnergyServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bw5EpicEnergyServicesApplication.class, args);
+	}
+
+}
