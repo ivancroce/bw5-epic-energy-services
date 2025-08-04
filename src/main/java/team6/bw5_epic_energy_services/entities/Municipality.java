8fdd4b9803c7a45f10ff1903270a2d2302afee11
IC Tech <148203290+ivancroce@users.noreply.github.com>
2025-08-04 16:55:36 +0100

    add entities and relations

---
+++ src/main/java/team6/bw5_epic_energy_services/entities/Municipality.java
@@ -0,0 +1,36 @@
+package team6.bw5_epic_energy_services.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "municipalities")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Municipality {
+    @Id
+    @GeneratedValue
+    @Setter(AccessLevel.NONE)
+    private UUID id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false, name = "province_code")
+    private String provinceCode;
+    @Column(nullable = false, name = "progressive_municipality_code")
+    private String progressiveMunicipalityCode;
+    @Column(nullable = false, name = "province_name")
+    private String provinceName;
+
+    @ManyToOne
+    @JoinColumn(name = "provice_id", nullable = false)
+    private Province province;
+
+    public Municipality(String name, Province province) {
+        this.name = name;
+        this.province = province;
+    }
+}
