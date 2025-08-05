53150a88a03953cf9d8a4135981b79faebab5994
Christian Ragonese <christianragonese1313@gmail.com>
2025-08-05 11:17:14 +0200

    save

---
+++ src/main/java/team6/bw5_epic_energy_services/services/ProvinceService.java
@@ -0,0 +1,12 @@
+package team6.bw5_epic_energy_services.services;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import team6.bw5_epic_energy_services.repositories.ProvinceRepository;
+
+@Service
+@Slf4j
+public class ProvinceService {
+    private ProvinceRepository provinceRepository;
+}
