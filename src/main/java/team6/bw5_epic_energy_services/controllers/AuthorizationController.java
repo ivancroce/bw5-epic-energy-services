13b138690c5cdc5a16bfef84e6529c36f03fac90
ZairaStra <straticozaira@gmail.com>
2025-08-06 16:17:22 +0200

    fixed login

--- src/main/java/team6/bw5_epic_energy_services/controllers/AuthorizationController.java
+++ src/main/java/team6/bw5_epic_energy_services/controllers/AuthorizationController.java
@@ -1,8 +1,12 @@
 package team6.bw5_epic_energy_services.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import team6.bw5_epic_energy_services.payloads.UserLoginDTO;
+import team6.bw5_epic_energy_services.payloads.UserLoginRespDTO;
 import team6.bw5_epic_energy_services.services.AuthorizationService;
 import team6.bw5_epic_energy_services.services.UserService;
 
@@ -17,9 +21,9 @@ public class AuthorizationController {
     public AuthorizationService authorizationsService;
 
     //LOGIN
-//    @PostMapping("/login")
-//    public UserLoginResponseDTO login(@RequestBody UserLoginDTO payload) {
-//        String extractedToken = authorizationsService.checkEmailBeforeLogin(payload);
-//        return new UserLoginResponseDTO(extractedToken);
-//    }
+    @PostMapping("/login")
+    public UserLoginRespDTO login(@RequestBody UserLoginDTO payload) {
+        String extractedToken = authorizationsService.checkEmailBeforeLogin(payload);
+        return new UserLoginRespDTO(extractedToken);
+    }
 }
