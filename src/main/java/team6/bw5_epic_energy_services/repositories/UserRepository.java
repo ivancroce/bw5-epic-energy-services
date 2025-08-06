b13f1c97c58d9f1d6ed9f4cc894f176719209b22
IC Tech <148203290+ivancroce@users.noreply.github.com>
2025-08-06 16:45:26 +0100

    add userrepo

--- src/main/java/team6/bw5_epic_energy_services/repositories/UserRepository.java
+++ src/main/java/team6/bw5_epic_energy_services/repositories/UserRepository.java
@@ -1,8 +1,15 @@
 package team6.bw5_epic_energy_services.repositories;
 
-/*@Repository
-public interface UserRepository extends JpaRepository<User, UUID> {
-    Optional<User> findByIdEmail(String email);
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import team6.bw5_epic_energy_services.entities.User;
 
-    Optional<User> findByIdUsername(String username);
-}*/
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsername(String username);
+}
