340c769e935a43ee961af22fbf3a6306df17b4f4
ZairaStra <straticozaira@gmail.com>
2025-08-06 12:38:12 +0200

    commented not working queries

--- src/main/java/team6/bw5_epic_energy_services/controllers/InvoiceController.java
+++ src/main/java/team6/bw5_epic_energy_services/controllers/InvoiceController.java
@@ -2,7 +2,10 @@ package team6.bw5_epic_energy_services.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import team6.bw5_epic_energy_services.entities.Invoice;
 import team6.bw5_epic_energy_services.services.InvoiceService;
 
@@ -15,61 +18,61 @@ public class InvoiceController {
     @Autowired
     private InvoiceService invoicesService;
 
-    @GetMapping("/customer/{customerId}") // GET http://localhost:3001/invoices/customer/{customerId}
-    public Page<Invoice> filterByCustomer(
-            @PathVariable UUID customerId,
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size,
-            @RequestParam(defaultValue = "id") String sortBy,
-            @RequestParam(defaultValue = "asc") String direction
-    ) {
-        return this.invoicesService.filterByCustomer(customerId, page, size, sortBy, direction);
-    }
-
-    @GetMapping("/status/{statusId}")// GET http://localhost:3001/invoices/status/{statusId}
-    public Page<Invoice> filterByStatus(
-            @PathVariable UUID statusId,
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size,
-            @RequestParam(defaultValue = "id") String sortBy,
-            @RequestParam(defaultValue = "asc") String direction
-    ) {
-        return invoicesService.filterByStatus(statusId, page, size, sortBy, direction);
-    }
-
-    @GetMapping("/{date}")// GET http://localhost:3001/invoices/{date}
-    public Page<Invoice> filterByDate(
-            @PathVariable LocalDate date,
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size,
-            @RequestParam(defaultValue = "id") String sortBy,
-            @RequestParam(defaultValue = "asc") String direction
-    ) {
-        return invoicesService.filterByDate(date, page, size, sortBy, direction);
-    }
-
-    @GetMapping("/{ye}")// GET http://localhost:3001/invoices/{year}
-    public Page<Invoice> findByDateYear(
-            @PathVariable int year,
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size,
-            @RequestParam(defaultValue = "id") String sortBy,
-            @RequestParam(defaultValue = "asc") String direction
-    ) {
-        return invoicesService.findByDateYear(year, page, size, sortBy, direction);
-    }
-
-    @GetMapping("/between/{min}/{max}") // GET http://localhost:3001/invoices/{min}/{max}
-    public Page<Invoice> findByAmountBetween(
-            @PathVariable Double min,
-            @PathVariable Double max,
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size,
-            @RequestParam(defaultValue = "id") String sortBy,
-            @RequestParam(defaultValue = "asc") String direction
-    ) {
-        return invoicesService.findByAmountBetween(min, max, page, size, sortBy, direction);
-    }
+//    @GetMapping("/customer/{customerId}") // GET http://localhost:3001/invoices/customer/{customerId}
+//    public Page<Invoice> filterByCustomer(
+//            @PathVariable UUID customerId,
+//            @RequestParam(defaultValue = "0") int page,
+//            @RequestParam(defaultValue = "10") int size,
+//            @RequestParam(defaultValue = "id") String sortBy,
+//            @RequestParam(defaultValue = "asc") String direction
+//    ) {
+//        return this.invoicesService.filterByCustomer(customerId, page, size, sortBy, direction);
+//    }
+//
+//    @GetMapping("/status/{statusId}")// GET http://localhost:3001/invoices/status/{statusId}
+//    public Page<Invoice> filterByStatus(
+//            @PathVariable UUID statusId,
+//            @RequestParam(defaultValue = "0") int page,
+//            @RequestParam(defaultValue = "10") int size,
+//            @RequestParam(defaultValue = "id") String sortBy,
+//            @RequestParam(defaultValue = "asc") String direction
+//    ) {
+//        return invoicesService.filterByStatus(statusId, page, size, sortBy, direction);
+//    }
+//
+//    @GetMapping("/{date}")// GET http://localhost:3001/invoices/{date}
+//    public Page<Invoice> filterByDate(
+//            @PathVariable LocalDate date,
+//            @RequestParam(defaultValue = "0") int page,
+//            @RequestParam(defaultValue = "10") int size,
+//            @RequestParam(defaultValue = "id") String sortBy,
+//            @RequestParam(defaultValue = "asc") String direction
+//    ) {
+//        return invoicesService.filterByDate(date, page, size, sortBy, direction);
+//    }
+//
+//    @GetMapping("/{ye}")// GET http://localhost:3001/invoices/{year}
+//    public Page<Invoice> findByDateYear(
+//            @PathVariable int year,
+//            @RequestParam(defaultValue = "0") int page,
+//            @RequestParam(defaultValue = "10") int size,
+//            @RequestParam(defaultValue = "id") String sortBy,
+//            @RequestParam(defaultValue = "asc") String direction
+//    ) {
+//        return invoicesService.findByDateYear(year, page, size, sortBy, direction);
+//    }
+//
+//    @GetMapping("/between/{min}/{max}") // GET http://localhost:3001/invoices/{min}/{max}
+//    public Page<Invoice> findByAmountBetween(
+//            @PathVariable Double min,
+//            @PathVariable Double max,
+//            @RequestParam(defaultValue = "0") int page,
+//            @RequestParam(defaultValue = "10") int size,
+//            @RequestParam(defaultValue = "id") String sortBy,
+//            @RequestParam(defaultValue = "asc") String direction
+//    ) {
+//        return invoicesService.findByAmountBetween(min, max, page, size, sortBy, direction);
+//    }
 
     @GetMapping("/search")
     public Page<Invoice> searchInvoices(
