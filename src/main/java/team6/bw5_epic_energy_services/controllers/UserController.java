9ae5441e387d58fad28e6782b91142683edbdecf
gaetanopyro <gaetano.miscio@gmail.com>
2025-08-07 10:08:31 +0200

    add save

--- src/main/java/team6/bw5_epic_energy_services/controllers/UserController.java
+++ src/main/java/team6/bw5_epic_energy_services/controllers/UserController.java
@@ -10,6 +10,8 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import team6.bw5_epic_energy_services.entities.User;
 import team6.bw5_epic_energy_services.exceptions.ValidationException;
+import team6.bw5_epic_energy_services.payloads.UserRegistrationDTO;
+import team6.bw5_epic_energy_services.payloads.UserRespDTO;
 import team6.bw5_epic_energy_services.payloads.UserUpdateDTO;
 import team6.bw5_epic_energy_services.services.UserService;
 
@@ -38,6 +40,19 @@ public class UserController {
     @GetMapping("/me")
     public User getMyProfile(@AuthenticationPrincipal User currentUser) {
         return currentUser;
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserRespDTO createNewUser(@RequestBody @Validated UserRegistrationDTO body, BindingResult validationResult) {
+        if (validationResult.hasErrors()) {
+            List<String> errors = validationResult.getAllErrors().stream()
+                    .map(objectError -> objectError.getDefaultMessage())
+                    .toList();
+            throw new ValidationException(errors);
+        }
+        return userService.save(body);
     }
 
     @PutMapping("/{userId}")
